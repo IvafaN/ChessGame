d0c9e6bae6b2f74230df7fefe6ed2003d30bb8c7
Lucas Gerhardt <lucasgerhardt3@hotmail.com>
2020-03-19 11:56:27 -0700

    Add descriptions

--- src/ChessGame/King.java
+++ src/ChessGame/King.java
@@ -16,26 +16,57 @@ class King extends Piece{
         setPosition(position);
         setNumMaxMovements(numMaxMovements);
     }
-
+    /**
+     * Setters...
+     * Change(update) the values of number of movements, the player(String) and color(boolean), the position and the symbol.
+     *
+     * @param value, isWhite, position, symbol, player
+     * @return
+     */
     @Override protected void    setNumMaxMovements   (int value) { numMaxMovements    = value; }
     @Override protected void    setIsWhite (boolean isWhite)     { this.isWhite  = isWhite; }
     @Override protected void    setPosition(Position position)   { this.position = position; }
     @Override protected void    setSymbol  (String symbol)       { this.symbol   = symbol; }
     @Override protected void    setPlayer  (String player)       { this.player   = player; }
-
+    /**
+     * Getters...
+     * Return the number of movements, the player(string) and color(boolean), the position and the symbol.
+     *
+     * @param
+     * @return numMaxMovements, isWhite, position, symbol, player
+     */
     @Override public    int      getNumMaxMovements() { return this.numMaxMovements; }
     @Override public    boolean  getIsWhite()         { return this.isWhite; }
     @Override public    Position getPosition()        { return this.position; }
     @Override public    String   getSymbol()          { return this.symbol; }
     @Override public    String   getPlayer()          { return this.player; }
-
+    /**
+     *
+     * @param
+     * @return
+     */
     @Override public void move() { System.out.println("One square"); }
+    /**
+     *
+     * @param
+     * @return
+     */
     @Override public String toString() { return getSymbol(); }
+    /**
+     *
+     * @param
+     * @return
+     */
     @Override public Boolean equals(Piece piece) {
         return (this.symbol  == piece.getSymbol()  &&
                 this.isWhite == piece.getIsWhite() &&
                 this.player  == piece.getPlayer() );
     }
+    /**
+     *
+     * @param newPosition is the new row and new col of the board
+     * @return if it's a valid move
+     */
     @Override public boolean isValidMove(Position newPosition){
         if(!super.isValidMove(position))return false; // First call the parent's method to check for the board bounds
 
